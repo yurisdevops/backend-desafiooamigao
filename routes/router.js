f7d55ec8d7e4475242627a540921a3e2abc1941f
Yuri Souza <143803323+yurisdevops@users.noreply.github.com>
2025-02-14 12:23:24 -0300

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -144,7 +144,7 @@ router.get("/members", (req, res) => {
 });
 
 router.delete("/members/:id", (req, res) => {
-  const id = req.query.id
+  const id = req.params.id
   if (!id) {
     return res.status(400).json({ error: "Id do membro ausente" });
   }
